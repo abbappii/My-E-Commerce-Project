587383883981e68f61e63ee664957599b8eff82f
Bappi <bappi142434@gmail.com>
2021-03-03 12:02:38 +0600

    first commit

---
+++ App_Login/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from App_Login import views
+
+app_name = 'App_Login'
+
+urlpatterns = [
+    path('sign_up/',views.sign_up,name='sign_up'),
+    path('login/',views.login_user, name = 'login'),
+    path('logout/',views.logout_user , name = 'logout'),
+    path('profile/',views.user_profile, name='profile'),
+]
